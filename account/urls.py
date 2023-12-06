e1f88e194cc78cb95dfd091018a502e3941985b8
mb <nerooren4001@gmail.com>
2023-12-06 23:24:44 +0300

    Add endpoints and templates for password change

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
 from account import views
 
+
 urlpatterns = [
+    # предыдущий url-адрес входа
     # path('login/', views.user_login, name='login'),
+    # url-адреса входа и выхода
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard')
+    # url-адреса смены пароля
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('', views.dashboard, name='dashboard'),
 ]
