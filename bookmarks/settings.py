9d95066859681266c4d21cd1db82016c4302cf5c
mb <nerooren4001@gmail.com>
2023-12-06 23:43:11 +0300

    Add password_change_done template

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'account',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
