5e852a370addb1e6d3836b995b611bc14c0f5cff
mbgoodguy <nerooren4001@gmail.com>
2023-12-14 00:31:28 +0300

    Add user follow/unfollow functionality
    Create user_list, user_detail and user_follow views
    
    Create Contact model for ManyToMany relationship-
    
    Used add_to_class() for dynamic add field into User model

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -160,3 +162,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.user.user_details',
     'account.authentication.create_profile',
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
